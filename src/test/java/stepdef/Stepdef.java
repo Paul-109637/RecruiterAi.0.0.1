b8191e7271bc44e50599cf644f9711c6d914210b
Paul-109637 <113929951+Paul-109637@users.noreply.github.com>
2022-10-23 21:39:20 +0530

    Update Stepdef.java

--- src/test/java/stepdef/Stepdef.java
+++ src/test/java/stepdef/Stepdef.java
@@ -65,7 +65,7 @@ public class Stepdef {
 		public void recruiter_clicks_on_Jobs_Menu() throws Throwable {
 			//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			//driver.findElement(By.xpath("(//a[@class='bx--header__menu-item'])[2]")).click();
-			Thread.sleep(2000);
+			Thread.sleep(4000);
 			driver.findElement(By.linkText("Jobs")).click();
 		}
 
