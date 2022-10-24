daaf2b772794be551ce54610c1cc844df4d8b637
Paul-109637 <113929951+Paul-109637@users.noreply.github.com>
2022-10-24 07:54:36 +0530

    Update Stepdef.java

--- src/test/java/stepdef/Stepdef.java
+++ src/test/java/stepdef/Stepdef.java
@@ -67,8 +67,9 @@ public class Stepdef {
 		@Given("^Recruiter clicks on Jobs Menu$")
 		public void recruiter_clicks_on_Jobs_Menu() throws Throwable {
 		//Thread.sleep(2000);	
-               driver.findElement(By.xpath("(//b[contains(text(),'Jobs')])[1]")).click();
-
+              // driver.findElement(By.xpath("(//b[contains(text(),'Jobs')])[1]")).click();
+                      Thread.sleep(3000);	
+	              driver.findElement(By.xpath("/html/body/section/div[1]/div/header/nav/ul/li[2]/a")).click();   
 			//driver.findElement(By.xpath("/html/body/section/div[1]/div/header/nav/ul/li[2]/a/span/b")).click();
 			//driver.findElement(By.xpath("(//a[@class='bx--header__menu-item'])[2]")).click();
 			//driver.findElement(By.linkText("Jobs")).click();
